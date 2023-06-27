eb3690ca341b2f13c8bdbd3a395e0af9722826ee
Junpeng.Li <1635975742@qq.com>
2023-06-27 10:29:51 +0800

    add: element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// @ts-ignore element-plus 自动导入组件样式
+import ElementPlus from 'unplugin-element-plus/vite'
+
 export default defineConfig({
   plugins: [
     vue(),
+    ElementPlus({})
   ],
   resolve: {
     alias: {

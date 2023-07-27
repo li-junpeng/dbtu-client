624d7197115019b2b5e3cec6f19b1ec86221008b
Junpeng.Li <1635975742@qq.com>
2023-07-27 22:54:00 +0800

    添加highlight.js, 并封装了sql-code-preview组件

---
+++ src/components/ui/sql-code-preview/index.vue
@@ -0,0 +1,64 @@
+<!--
+ * 代码预览组件
+ *
+ * @author Junpeng.Li
+ * @date 2023-07-27 22:
+-->
+<script setup lang="ts">
+import { computed } from 'vue'
+import { ElScrollbar } from 'element-plus'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/vs.css'
+
+defineOptions({
+  name: 'SqlCodePreviewComponent'
+})
+
+const props = defineProps({
+  code: {
+    type: String,
+    required: true
+  }
+})
+const codeHtml = computed(() => {
+  return hljs.highlight(props.code, { language: 'sql' }).value
+})
+</script>
+
+<template>
+  <div class="dbtu-code-preview">
+    <el-scrollbar always>
+      <pre class="pre-box">
+        <code ref="codeRef" class="language-sql" v-html="codeHtml"></code>
+      </pre>
+    </el-scrollbar>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dbtu-code-preview {
+  width: 100%;
+  height: 100%;
+  padding: 10px 0;
+
+  .pre-box {
+    white-space: pre-line;
+    line-height: 26px;
+    padding: 0 10px;
+
+    code {
+      color: var(--dbtu-font-color);
+      font-family: Consolas, "Courier New", monospace;
+
+      .hljs-built_in, .hljs-keyword, .hljs-name, .hljs-selector-tag, .hljs-tag {
+        color: #0064ff;
+      }
+
+      .hljs-addition, .hljs-attribute, .hljs-literal, .hljs-section, .hljs-string,
+      .hljs-template-tag, .hljs-template-variable, .hljs-title, .hljs-type {
+        color: #af670a;
+      }
+    }
+  }
+}
+</style>

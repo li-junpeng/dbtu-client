9480ff71286028d09b9cadfe6dd45f7c1bfbce74
Junpeng <1635975742@qq.com>
2023-08-08 16:01:26 +0800

    feat: 在popover中展示编辑表格组件

--- src/components/ui/edit-table-popover/index.ts
+++ src/components/ui/edit-table-popover/index.ts
@@ -1,8 +1,122 @@
+import type { TableColumnCtx } from "element-plus"
+
+export type TableColumn = TableColumnCtx<unknown>
+export type TableRowItem = any
+
 export interface EditTablePopoverSlots {
-  reference(props: { open: () => void }): void
+  // 与 el-popover 的reference插槽一样
+  reference(): void,
+
+  // 动态插槽, 插槽名: #column-${column.prop}
+  [key: string]: (row: TableRowItem, column: TableColumnOption, currentRow: TableRowItem, currentColumn: TableColumn) => void
+}
+
+export type TableColumnComponentType = 'text' | 'slot' | 'input' | 'input-number' | 'checkbox' | 'switch' | 'select'
+
+/**
+ * el-table-column 属性
+ */
+export interface TableColumnOption {
+  /**
+   * el-table-column of label
+   */
+  label: string
+
+  /**
+   * el-table-column of prop
+   */
+  prop: string
+
+  /**
+   * 单元格需要以什么组件来编辑值
+   * 
+   * - text: 纯文本展示
+   * - slot: 自定义插槽, 插槽名为prop
+   */
+  component: TableColumnComponentType
+
+  /**
+   * 单元格对齐方式
+   */
+
+  align?: 'left' | 'right' | 'center'
+
+  /**
+   * checkbox属性, 当`component = 'checkbox'`时必填
+   */
+  checkbox?: {
+    // 选中时的值
+    trueValue: number | string
+    // 未选中时的值, 默认为空字符串
+    falseValue?: number | string
+  }
+
+  /**
+   * switch属性, 当`component = 'switch'`时必填
+   */
+  switch?: {
+    // 打开时的值, 默认为true
+    trueValue?: number | string | boolean
+    // 关闭时的值, 默认为false
+    falseValue?: number | string | boolean
+  }
+
+  /**
+   * select属性, 当`component = 'select'`时必填
+   */
+  select?: {
+    // el-option 数据, 如果是普通类型的数组, 则不需要配置valueKey和labelKey
+    // 如果是对象数组, 则valueKey和labelKey必须要配置
+    options: any[],
+    valueKey?: string,
+    labelKey?: string,
+    clearable?: boolean
+    filterable?: boolean
+    placeholder?: string
+  }
 }
 
 export interface EditTablePopoverProps {
+
+  /**
+   * 数据项的key, 用来标识哪个字段是唯一值
+   */
+  rowKey: string
+
+  /**
+   * [添加项]按钮的回调函数, 请自定义添加项的业务代码
+   * 
+   * @returns 返回需要添加的数据
+   * 
+   * @example
+   * 
+   * <script setup lang="ts">
+   * const fields = [
+   *    {
+   *      id: 1,
+   *      field: 'name,
+   *    }
+   * ]
+   * 
+   * const addItem = () => {
+   *    return {
+   *      id: 2,
+   *      field: 'username'
+   *    }
+   * }
+   * </script>
+   * 
+   * <template>
+   *    <EditTablePopover v-model="fields" :add-item="addItem"></EditTablePopover>
+   * </template>
+   */
+  addItem: () => any
+
+  /**
+   * 表格列, 也可以通过插槽#columns自定义
+   */
+  columns: TableColumnOption[]
+
   /**
    * el-popover的宽度, 单位: px
    * 
@@ -20,13 +134,12 @@ export interface EditTablePopoverProps {
   /**
    * el-popover placement
    * 
-   * @default 'top'
+   * @default undefined
    */
-  placement: 'top' | 'bottom' | 'left' | 'right'
+  placement?: 'top' | 'bottom' | 'left' | 'right'
 }
 
-export const EditTablePopoverPropDefaults: EditTablePopoverProps = {
+export const EditTablePopoverPropDefaults: Partial<EditTablePopoverProps> = {
   width: 600,
-  height: 400,
-  placement: 'top'
+  height: 400
 }
--- src/components/ui/edit-table-popover/index.vue
+++ src/components/ui/edit-table-popover/index.vue
@@ -6,13 +6,21 @@
  * @date 2023-08-07 23:21
 -->
 <script setup lang="ts">
+import type { CSSProperties } from 'vue'
+import { ElInput } from 'element-plus'
+import type {
+  EditTablePopoverProps,
+  EditTablePopoverSlots,
+  TableColumnOption,
+  TableColumnComponentType,
+  TableColumn,
+  TableRowItem
+} from '.'
 import { reactive, ref } from 'vue'
-import type { TableColumnCtx } from 'element-plus'
-import type { EditTablePopoverProps, EditTablePopoverSlots } from '.'
+import { useElementVisibility } from '@vueuse/core'
 import { EditTablePopoverPropDefaults } from '.'
-
-type TableColumn = TableColumnCtx<unknown>
-type TableRowItem = any
+import { ArrayUtils } from '@/common/utils/ArrayUtils'
+import { useComponentRef } from '@/components/element-plus/element-plus-util'
 
 defineOptions({
   name: 'EditTablePopoverComponent'
@@ -20,6 +28,7 @@ defineOptions({
 
 defineSlots<EditTablePopoverSlots>()
 
+// @ts-ignore
 const props = withDefaults(defineProps<EditTablePopoverProps>(), EditTablePopoverPropDefaults)
 
 const popover = reactive({
@@ -38,9 +47,106 @@ const onClickRow = (row: TableRowItem, column: TableColumn) => {
   selectedColumn.value = column
 }
 
-const open = () => {
-  popover.visible = true
+// el-table height
+const tableHeight = computed<string>(() => {
+  // 总高度 - 底部高度(34px) - 底部的margin-top(10px)
+  return `${props.height - 44}px`
+})
+
+// popover容器样式
+const containerStyle = computed(() => {
+  return {
+    width: '100%',
+    height: `${props.height}px`
+  } as CSSProperties
+})
+
+// 添加项
+const addItem = () => {
+  const item = props.addItem?.()
+  if (item === null || item === void 0) {
+    return
+  }
+
+  tableRows.value.push(item)
+  selectedRow.value = tableRows.value[tableRows.value.length - 1]
 }
+
+// 删除项
+const deleteItem = () => {
+  const b = ArrayUtils.remove(tableRows.value, selectedRow.value?.[props.rowKey], props.rowKey)
+  if (b) {
+    if (tableRows.value.length === 0) {
+      addItem()
+    } else {
+      selectedRow.value = tableRows.value[0]
+    }
+  }
+}
+
+// 上移
+const moveTop = () => {
+  ArrayUtils.moveTop(tableRows.value, selectedRow.value, props.rowKey)
+}
+
+// 下移
+const moveBottom = () => {
+  ArrayUtils.moveBottom(tableRows.value, selectedRow.value, props.rowKey)
+}
+
+// 单元格内表单组件的实例
+const inputRef = useComponentRef(ElInput)
+const inputNumberRef = useComponentRef(ElInputNumber)
+
+// 是否显示单元格内的表单组件
+const isShowComponent = (
+  column: TableColumnOption,
+  component: TableColumnComponentType,
+  row: TableRowItem
+) => {
+  return (
+    selectedRow.value?.[props.rowKey] === row[props.rowKey] &&
+    column.component === component &&
+    selectedColumn.value?.property === column.prop
+  )
+}
+
+// 点击单元格时, 使其变为可编辑状态
+watch(
+  () => [selectedRow.value, selectedColumn.value],
+  () => {
+    nextTick(() => {
+      const prop = selectedColumn.value?.property
+      const column = props.columns.find(item => item.prop === prop)
+      if (column) {
+        switch (column.component) {
+          case 'input':
+            // @ts-ignore
+            inputRef.value?.[0].focus()
+            break
+          case 'input-number':
+            // @ts-ignore
+            inputNumberRef.value?.[0].focus()
+            break
+        }
+      }
+    })
+  }
+)
+
+// #region 在滚动或者切换tab选项卡时, 使popover隐藏
+const mainRef = ref()
+const mainVisible = useElementVisibility(mainRef)
+watch([mainVisible], () => {
+  if (!mainVisible.value) {
+    popover.visible = false
+  }
+})
+// #endregion
+
+onMounted(() => {
+  addItem()
+})
 </script>
 
 <template>
@@ -56,37 +162,193 @@ const open = () => {
     >
       <template #reference>
         <!-- 用来触发显示popover的插槽 -->
-        <slot
-          name="reference"
-          :open="open"
+        <div
+          ref="mainRef"
+          @click="popover.visible = true"
         >
-          <el-button
-            ref="buttonRef"
-            text
-            link
-            @click="open"
-            style="padding: 0 10px"
-          >
-            <template #icon>
-              <IconMoreFilled />
-            </template>
-          </el-button>
-        </slot>
+          <slot name="reference">
+            <el-button
+              ref="buttonRef"
+              text
+              link
+              style="padding: 0 10px"
+            >
+              <template #icon>
+                <IconMoreFilled />
+              </template>
+            </el-button>
+          </slot>
+        </div>
       </template>
 
-      <div class="popover-container">
+      <div
+        class="popover-container"
+        :style="containerStyle"
+      >
         <el-table
           :data="tableRows"
           border
-          height="256px"
+          :height="tableHeight"
           highlight-current-row
-          :current-row-key="selectedRow?.id"
-          row-key="id"
+          :current-row-key="selectedRow?.[props.rowKey]"
+          :row-key="props.rowKey"
           scrollbar-always-on
           @row-click="onClickRow"
           class="el-table-editable"
         >
+          <!-- 序号列 -->
+          <el-table-column
+            type="index"
+            width="50px"
+            label="#"
+            align="center"
+            class-name="bg-is-theme-color"
+            style="cursor: pointer"
+          >
+            <template #default="{ $index }">
+              {{ $index + 1 }}
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            v-for="column in props.columns"
+            :key="column.prop"
+            :label="column.label"
+            :prop="column.prop"
+            :align="column.align || 'left'"
+          >
+            <template #default="{ row }">
+              <el-input
+                v-if="isShowComponent(column, 'input', row)"
+                ref="inputRef"
+                v-model="row[column.prop]"
+              />
+
+              <el-input-number
+                v-else-if="isShowComponent(column, 'input-number', row)"
+                ref="inputNumberRef"
+                v-model="row[column.prop]"
+                :controls="false"
+                class="el-input-number__text-left"
+                style="width: 100%"
+              />
+
+              <el-checkbox
+                v-else-if="column.component === 'checkbox'"
+                v-model="row[column.prop]"
+                :true-label="column.checkbox?.trueValue"
+                :false-label="column.checkbox?.falseValue || ''"
+              />
+
+              <el-switch
+                v-else-if="column.component === 'switch'"
+                size="small"
+                v-model="row[column.prop]"
+                :active-value="column.switch?.trueValue || true"
+                :inactive-value="column.switch?.falseValue || false"
+              />
+
+              <el-select
+                v-else-if="isShowComponent(column, 'select', row)"
+                v-model="row[column.prop]"
+                :clearable="column.select?.clearable || false"
+                :filterable="column.select?.filterable || false"
+                :placeholder="column.select?.placeholder || ' '"
+                style="width: 100%"
+              >
+                <el-option
+                  v-for="item in column.select?.options"
+                  :key="column.select!.valueKey ? item[column.select!.valueKey] : item"
+                  :value="column.select!.valueKey ? item[column.select!.valueKey] : item"
+                  :label="column.select!.labelKey ? item[column.select!.labelKey] : item"
+                />
+              </el-select>
+
+              <!-- 动态插槽, 插槽名: column-${column.prop} -->
+              <slot
+                v-else-if="column.component === 'slot'"
+                :name="`column-${column.prop}`"
+                :column="column"
+                :row="row"
+                :currentRow="selectedRow"
+                :currentColumn="selectedColumn"
+              >
+                <div style="color: #f00">请自定义插槽: #column-{{ column.prop }}</div>
+              </slot>
+
+              <!-- 文本显示 -->
+              <span
+                v-else
+                class="row-readonly-text"
+              >
+                {{ row[column.prop] }}+
+              </span>
+            </template>
+          </el-table-column>
+
+          <!-- <slot name="columns" :current-row="selectedRow">
+          </slot> -->
         </el-table>
+
+        <div class="footer">
+          <div>
+            <el-button
+              text
+              link
+              @click="addItem"
+            >
+              <template #icon>
+                <IconPlus />
+              </template>
+              添加项
+            </el-button>
+
+            <el-button
+              text
+              link
+              :disabled="!selectedRow"
+              @click="deleteItem"
+            >
+              <template #icon>
+                <IconDelete />
+              </template>
+              删除项
+            </el-button>
+
+            <el-button
+              text
+              link
+              :disabled="!selectedRow"
+              @click="moveTop"
+            >
+              <template #icon>
+                <IconTop />
+              </template>
+              上移
+            </el-button>
+
+            <el-button
+              text
+              link
+              :disabled="!selectedRow"
+              @click="moveBottom"
+            >
+              <template #icon>
+                <IconBottom />
+              </template>
+              下移
+            </el-button>
+          </div>
+
+          <div>
+            <el-button
+              type="info"
+              @click="popover.visible = false"
+            >
+              <span>关闭</span>
+            </el-button>
+          </div>
+        </div>
       </div>
     </el-popover>
   </div>

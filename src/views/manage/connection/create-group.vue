fc868d7d8f45faef67a410cf9514c00901d47028
Junpeng <1635975742@qq.com>
2023-08-15 15:55:44 +0800

    perf: 创建分组对话框使用`RenameDialog`组件代替

--- src/views/manage/connection/create-group.vue
+++ src/views/manage/connection/create-group.vue
@@ -5,11 +5,8 @@
  * @date 2023-07-14 17-06
 -->
 <script setup lang="ts">
-import { StringUtils } from '@/common/utils/StringUtils'
-import { useComponentRef } from '@/components/element-plus/element-plus-util'
 import { useConnectionStore } from '@/stores/ConnectionStore'
-import { Message } from '@/components/element-plus/el-feedback-util'
-import { MessageBox } from '@element-plus/icons-vue'
+import RenameDialog, { type RenameDialogInstance } from '@/components/ui/rename-dialog'
 
 defineOptions({
   name: 'CreateGroupComponent'
@@ -19,76 +16,52 @@ const emits = defineEmits<{
   (e: 'submit-success', data: ConnectionGroup): void
 }>()
 
+const renameDialogRef = ref<RenameDialogInstance>()
+
 const connectionStore = useConnectionStore()
-const formRef = useComponentRef(ElForm)
 const dialog = reactive({
-  visible: false,
-  title: '创建分组',
   isEdit: false
 })
+
 const formData = ref<ConnectionGroup>({
   id: null,
   name: '',
   nodeType: 'group'
 })
+
 const formRules = {
-  name: [
-    {
-      trigger: 'blur',
-      validator: (a: any, b: any, callback: Function) => {
-        if (StringUtils.isEmpty(formData.value.name)) {
-          throw new Error('分组名称为必填项')
-        }
-        callback()
-      }
-    }
-  ]
+  required: true,
+  trigger: 'blur',
+  message: '分组名称为必填项'
 }
 
 const open = (data?: ConnectionGroup) => {
   if (data) {
     dialog.isEdit = true
-    dialog.title = '编辑分组'
     formData.value = JSON.parse(JSON.stringify(data))
+    renameDialogRef.value?.open(formData.value.name)
   } else {
     dialog.isEdit = false
-    dialog.title = '创建分组'
     formData.value = {
       id: null,
       name: 'New Group',
       nodeType: 'group'
     }
+    renameDialogRef.value?.open('New Group')
   }
-
-  dialog.visible = true
 }
 
-const onCancel = () => {
-  dialog.visible = false
-}
+const onConfirm = async (name: string): Promise<string> => {
+  formData.value.name = name
 
-const isSaving = ref(false)
-const submitBtnText = computed(() => {
-  return isSaving.value ? '正在保存' : dialog.isEdit ? '保存' : '创建'
-})
-const onConfirm = () => {
-  formRef.value?.validate(async valid => {
-    if (!valid) {
-      return
-    }
-
-    isSaving.value = true
-    const fn = dialog.isEdit ? connectionStore.updateGroupById : connectionStore.createGroup
-    const { status, message } = await fn(formData.value)
-    if (status === 'success') {
-      dialog.visible = false
-      Message.success(message)
-      emits('submit-success', formData.value)
-    } else {
-      MessageBox.error(message)
-    }
-    isSaving.value = false
-  })
+  const fn = dialog.isEdit ? connectionStore.updateGroupById : connectionStore.createGroup
+  const { status, message } = await fn(formData.value)
+  if (status === 'success') {
+    emits('submit-success', formData.value)
+    return Promise.resolve(message)
+  } else {
+    return Promise.reject(message)
+  }
 }
 
 defineExpose({
@@ -97,47 +70,12 @@ defineExpose({
 </script>
 
 <template>
-  <el-dialog
-    v-model="dialog.visible"
-    :title="dialog.title"
-    width="500px"
-    :close-on-click-modal="false"
-    destroy-on-close
-    draggable
-  >
-    <el-form
-      ref="formRef"
-      :model="formData"
-      :rules="formRules"
-      label-width="90px"
-      label-position="left"
-      @submit.prevent
-    >
-      <el-form-item
-        label="分组名称"
-        prop="name"
-      >
-        <el-input
-          v-model="formData.name"
-          maxlength="40"
-        />
-      </el-form-item>
-    </el-form>
-
-    <template #footer>
-      <el-button
-        type="info"
-        @click="onCancel"
-        >取消</el-button
-      >
-      <el-button
-        type="primary"
-        @click="onConfirm"
-        :loading="isSaving"
-        >{{ submitBtnText }}</el-button
-      >
-    </template>
-  </el-dialog>
+  <RenameDialog
+    ref="renameDialogRef"
+    :title="dialog.isEdit ? '重命名' : '创建分组'"
+    :validate-rule="formRules"
+    :confirm="onConfirm"
+    :confirm-text="dialog.isEdit ? '保存' : '创建'"
+    label="分组名"
+  />
 </template>
-
-<style scoped lang="scss"></style>
